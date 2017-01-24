d9150063f8e71f72c9ca291815596a1d897e7268
Daniel Villalba <dvillalba@tiempodevelopment.com>
2017-01-24 15:01:44 -0600

    Add project files.

---
+++ CustomerRenderedMap/CustomerRenderedMap/CustomizedMap/CustomMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+
+namespace CustomerRenderedMap
+{
+    public class CustomMap : Map
+    {
+        public List<CustomPin> CustomPins { get; set; }
+    }
+
+    public class CustomPin
+    {
+        public Pin Pin { get; set; }
+        public string Id { get; set; }
+        public string Url { get; set; }
+    }
+}
